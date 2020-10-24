9b58f6e5e2ec2cf321a12fd93c2881eaca2a8028
1721172 <d.gajjar@wlv.ac.uk>
2020-10-24 16:06:39 +0100

    First Commit replacing JPrograms repo

---
+++ src/ams/PlayButtonListener13.java
@@ -0,0 +1,14 @@
+package ams;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PlayButtonListener13 implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		new Test1Question6();
+
+	}
+
+}
